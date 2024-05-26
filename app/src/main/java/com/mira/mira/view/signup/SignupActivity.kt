7e22d76873a53490888a0847b7f3ac988a6bf1ed
er4files <rahmaderasugiarto@gmail.com>
2024-05-27 00:42:48 +0700

    Add Login and Signup Activity

---
+++ app/src/main/java/com/mira/mira/view/signup/SignupActivity.kt
@@ -0,0 +1,28 @@
+package com.mira.mira.view.signup
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mira.mira.R
+import android.widget.ImageView
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val backIcon = findViewById<ImageView>(R.id.back_icon)
+        backIcon.setOnClickListener {
+            finish()
+        }
+    }
+}
