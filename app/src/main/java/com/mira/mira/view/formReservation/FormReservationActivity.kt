e620354197bef1141b0445aa787692bdd4b958b9
Alexander Kenrick Duanto <alexanderkenrick3@gmail.com>
2024-06-03 20:59:06 +0700

    Revert "Add date logic at reservation"
    This reverts commit 2d6e30f216a5107ca99ba8a2e38868597c1396da.

--- app/src/main/java/com/mira/mira/view/formReservation/FormReservationActivity.kt
+++ app/src/main/java/com/mira/mira/view/formReservation/FormReservationActivity.kt
@@ -1,49 +1,21 @@
 package com.mira.mira.view.formReservation
 
-import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mira.mira.R
-import com.mira.mira.databinding.ActivityFormReservationBinding
-import com.mira.mira.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
-import java.util.Calendar
 
 class FormReservationActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityFormReservationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityFormReservationBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.dateTextView.text = intent.getStringExtra(RESERVATION_DATE)
-
-        binding.editTextDate.setOnClickListener {
-            val today = Calendar.getInstance()
-            val year = today.get(Calendar.YEAR)
-            val month = today.get(Calendar.MONTH)
-            val day = today.get(Calendar.DAY_OF_MONTH)
-            val context = applicationContext
-            val datePicker = DatePickerDialog(context,
-                DatePickerDialog.OnDateSetListener { datePicker, selYear, selMonth, selDay ->
-                    val calendar = Calendar.getInstance()
-                    calendar.set(selYear, selMonth, selDay)
-
-                    var dateFormat = SimpleDateFormat("dd-MM-yyyy")
-                    var str = dateFormat.format(calendar.time)
-                    this.binding.editTextDate.setText(str)
-                }, year, month, day
-            )
-            datePicker.show()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_form_reservation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
-
-    companion object{
-        const val RESERVATION_DATE = "reservation_date"
-    }
-
 }
