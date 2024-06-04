a9a1d2b243ab608c74525e489e7477f527ce2c8e
er4files <rahmaderasugiarto@gmail.com>
2024-06-04 12:28:42 +0700

    notif connect to cloud masange

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
- //firebase
-
+    //firebase
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation ("com.google.firebase:firebase-analytics-ktx")
     implementation ("com.google.firebase:firebase-auth-ktx:23.0.0")
@@ -88,6 +87,8 @@ dependencies {
     implementation ("com.google.firebase:firebase-firestore:25.0.0")
     implementation ("com.google.firebase:firebase-auth:23.0.0")
 
+    //notif
+    implementation ("com.google.firebase:firebase-messaging")
 
 
 }
