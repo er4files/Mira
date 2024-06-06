e8a1495f03ce287bd60c17d1b61e0a20f32a8b09
Alexander Kenrick Duanto <alexanderkenrick3@gmail.com>
2024-06-06 21:18:31 +0700

    Fix Date and Timer Picker

--- app/src/main/java/com/mira/mira/data/api/MiraApiService.kt
+++ app/src/main/java/com/mira/mira/data/api/MiraApiService.kt
@@ -7,10 +7,12 @@ import com.mira.mira.data.model.UserData
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.PATCH
+import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
 
@@ -37,4 +39,19 @@ interface MiraApiService {
         @Path("id") patientId: String,
         @Header("Authorization") token: String
     ): Call<List<Notification>>
+
+    @FormUrlEncoded
+    @POST("/add")
+    fun addForm(
+        @Header("Authorization") token: String,
+        @Part("nama_pasien") nama_pasien: String,
+        @Part("alamat") alamat : String,
+        @Part("tanggal_lahir") tanggal_lahir : String,
+        @Part("gender") gender : String,
+        @Part("no_hp") no_hp : String,
+        @Part("email") email : String,
+        @Part("tanggal_kunjungan") tanggal_kunjungan : String,
+        @Part("jam_kunjungan") jam_kunjungan : String,
+        @Part("jenis_periksa") jenis_periksa : String
+    ) : Call<UserData>
 }
